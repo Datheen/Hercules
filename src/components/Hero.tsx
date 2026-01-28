003edbf9084c04ac8cba067fc5f728309730967a
Datheen <praydatheen@gmail.com>
2026-01-28 05:27:06 -0400

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 import ckris from "../assets/img/hero/ckris.webp";
 import { ButtonAnim } from "./ui/ButtonAnim";
 import angular from "../assets/img/hero/angular.webp";
@@ -6,6 +8,23 @@ import next from "../assets/img/hero/Nextjs.webp";
 import tailwind from "../assets/img/hero/tailwind.webp";
 
 export function Hero() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const elementos = containerRef.current.querySelectorAll('[id="animar"]');
+      
+      elementos.forEach((elemento, index) => {
+        gsap.to(elemento, {
+          y: -15,
+          duration: 1.5 + (index * 0.2),
+          ease: "power1.inOut",
+          yoyo: true,
+          repeat: -1,
+        });
+      });
+    }
+  }, []);
   return (
     <div
       id="hero-background"
@@ -16,7 +35,7 @@ export function Hero() {
           id="hero-box"
           className="flex flex-col md:justify-between md:mx-34 md:flex-row justify-center text-center md:text-start items-center pt-30 md:pt-34"
         >
-          <div id="title" className="flex flex-col gap-2 -mt-10">
+          <div id="title" className="flex flex-col gap-2 md:-mt-10">
             <p className="md:text-2xl text-2xl text-white/70">Olá, eu sou</p>
             <h1 className="md:text-7xl text-5xl bg-linear-to-r from-lime-400 to-green-700 bg-clip-text text-transparent font-bold">
               Ckristian Felipe
@@ -36,11 +55,11 @@ export function Hero() {
               </ButtonAnim>
             </div>
           </div>
-          <div id="ckris" className="relative">
-            <img className="w-[71px]  absolute right-20 top-2" src={angular} />
-            <img className="w-[78px] absolute top-20" src={react} />
-            <img className="w-[71px] absolute right-0 top-40" src={next} />
-            <img className="w-[90px] absolute bottom-25" src={tailwind} />
+          <div id="ckris" className="relative md:mt-3 mt-26" ref={containerRef}>
+            <img id="animar" className="w-[71px] absolute right-20 top-2" src={angular} />
+            <img id="animar" className="w-[78px] absolute top-20" src={react} />
+            <img id="animar" className="w-[71px] absolute right-0 top-40" src={next} />
+            <img id="animar" className="w-[90px] absolute bottom-25" src={tailwind} />
             <img className="w-[390px] px-2" src={ckris} />
           </div>
         </div>
