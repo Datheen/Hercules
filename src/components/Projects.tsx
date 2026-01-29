d8d7970117daecab70e988b0b3af60c67d8a228e
Datheen <praydatheen@gmail.com>
2026-01-28 23:11:02 -0400

    update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,10 @@ import { Star } from "./ui/Star";
 export function Projects() {
   return (
     <div className="w-full bg-[#0b1b25] h-auto">
-      <div id="container-projects" className="max-w-360 flex flex-col px-4 mx-auto">
+      <div
+        id="container-projects"
+        className="max-w-360 flex flex-col px-4 mx-auto"
+      >
         <div className=" md:px-20 text-center flex flex-col gap-y-10">
           <h2 className="mt-20 font-bold text-[#7BCD00] mb-10 mx-auto text-3xl leading-[1.1] sm:text-3xl md:text-5xl">
             Últimos Projetos
@@ -15,20 +18,24 @@ export function Projects() {
           </p>
         </div>
         {/* Cards */}
-        <div id="card-sction" className="grid mt-20 md:flex mx-auto gap-7 lg:gap-10 md:w-[90%]">
+        <div
+          id="card-sction"
+          className="grid mt-20 md:flex mx-auto gap-7 lg:gap-10 md:w-[90%]"
+        >
           {data.map((item) => (
             <div
               id="card"
               key={item.id}
-              className="group shadow-2xl overflow-hidden bg-[url('@/assets/img/projects/project-1.webp')] border-1 border-white/80 active:scale-80 transition-all duration-700 bg-cover bg-center h-[370px] w-[380px] rounded-4xl"
+              className="group shadow-2xl overflow-hidden border border-[#7BCD00]/80 active:scale-80 transition-all duration-700 bg-cover bg-center h-92.5 w-95 rounded-4xl" style={{ backgroundImage: `url(${item.image})` }}
             >
               <div className="h-[56%] rounded-t-4xl">
-                <img id="imagem-card"
+                <img
+                  id="imagem-card"
                   className="bg-cover bg-center rounded-t-4xl transition-all duration-700 hover:scale-115 hover:cursor-pointer"
                   src={item.image}
                 />
               </div>
-              <div className="bg-zinc-900/65 backdrop-blur-xl border border-white/10 h-[172px] rounded-b-4xl px-6">
+              <div className="bg-zinc-900/65 backdrop-blur-xl border border-white/10 h-43 rounded-b-4xl px-6">
                 <div className="mt-3 flex gap-2" id="badges">
                   <div className={item.variant}>
                     <span className="flex items-center justify-center text-white/90">
